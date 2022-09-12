418b2c10249ea83a02fdc498a5b18831da9705b0
Resister-boy <hassanpumped17@gmail.com>
2022-09-12 23:48:40 +0900

    content detail 페이지 디자인

--- pages/contents/[id].tsx
+++ pages/contents/[id].tsx
@@ -47,100 +47,109 @@ interface Props {
 const renderBlock = (block: any) => {
    switch (block.type) {
     case 'paragraph': 
-       return <p>{block.paragraph.rich_text[0].text.content}</p> 
-    // case 'heading_1':
-    //     return <h1>{block.heading_1.rich_text[0].text.content}</h1>
-    // case 'heading_2':
-    //     return <h2>{block.heading_2.rich_text[0].text.content}</h2>
-    // case 'heading_3':
-    //     return <h3>{block.heading_3.rich_text[0].text.content}</h3>
-    // case 'heading_4':
-    //     return <h4>{block.heading_4.rich_text[0].text.content}</h4>
-    // case 'heading_5':
-    //     return <h5>{block.heading_5.rich_text[0].text.content}</h5>
-    // case 'numbered_list_item':
-    //     return <ol></ol>
+       return <div className='my-4'>{
+            typeof block.paragraph.rich_text[0] !== 'undefined'
+            ? <p className='text-md'>{block.paragraph.rich_text[0].text.content}</p>
+            : <div className='w-full h-2'></div>
+            }</div> 
+    case 'heading_1':
+        return <p className='text-3xl font-semibold'>{block.heading_1.rich_text[0].text.content}</p>
+    case 'heading_2':
+        return <p className='text-2xl font-semibold'>{block.heading_2.rich_text[0].text.content}</p>
+    case 'heading_3':
+        return <p className='text-xl font-semibold'>{block.heading_3.rich_text[0].text.content}</p>
+    case 'heading_4':
+        return <p className='text-lg font-semibold'>{block.heading_4.rich_text[0].text.content}</p>
+    case 'heading_5':
+        return <p className='text-base font-semibold'>{block.heading_5.rich_text[0].text.content}</p>
+    case 'bulleted_list_item': 
+        return (
+          <ul className='mt-8'>
+            <li className='list-[square] mt-4 text-base font-semibold ml-4'>{block.bulleted_list_item.rich_text[0].text.content}</li>
+          </ul>
+        )
+    case 'numbered_list_item':
+        return (
+          <ol className='mt-8'>
+            <li className='mt-4 text-lg font-normal'>{block.numbered_list.item.rich_text[0].text.content}</li>
+          </ol>
+        )
+    case 'code': 
+        return (
+            <div className='bg-[#E9E9E9] rounded-md'>
+              <p className='text-[#292929] py-4 px-8'>{block.code.rich_text[0].text.content}</p>
+            </div>
+          )
+    case 'bookmark': {
+        return (
+          <Link href={block.bookmark.url}>
+            <a className='my-4'>
+              {block.bookmark.url}
+            </a>
+          </Link>
+        )
+    }
     default: 
         return <p>{` `}</p>
    }
 }
     
-const Post:NextPage<Props> = ({id, post, blocks}) => {
+const ContentDetail:NextPage<Props> = ({id, post, blocks}) => {
   console.log(id)
   console.log(post)
   console.log(blocks)
-  console.log(blocks[0])
    return (
-     <div>
+     <section className='w-2/3 mx-auto my-12'>
        <Head>
          <title>
            {post.properties.Name.title[0].text.content}
          </title>
        </Head>
-       <div>
+      <div className='w-full mx-auto mt-12 px-16'>
+        <Image 
+          src={post.cover.external.url}
+          alt="Content Image"
+          width={900}
+          height={400}
+        />
+        <div className='flex mt-8 justify-between items-center'>
+          <span className='text-2xl font-semibold'>{post.properties.Name.title[0].text.content}</span> 
+          <span className='text-base text-[#ccc] font-thin'>#{post.properties.Series.select.name}</span>
+      </div>
+      <div className='mt-5 flex justify-between'>
+        <div className='flex'>
+            {post.properties.Keyword.multi_select.map((keyword: any, index: number) => {
+              return (
+                <span key={index} className="px-2 bg-slate-100 text-[#333] font-semobold rounded-md mr-2">{keyword.name}</span>
+              )
+            })}
+        </div>
+        <span className='text-base text-[#ccc]'>{post.properties.Date.date.start}</span>
+      </div>
+      <div className='w-4/5 mx-auto mt-12 pt-8 border-t-2'>
         {
-          renderBlock(blocks[0])
+          blocks.map((block, index) => {
+            return (
+              <div key={index}>
+                {
+                  renderBlock(block)
+                }
+              </div>
+            )})
         }
-       </div>
-       {/* {
-         blocks.map((block, index) => {
-           return (
-             <div key={index}>
-               {
-                 renderBlock(block)
-               }
-             </div>
-           )})
-       } */}
-     </div>
+      </div>
+      </div>
+     </section>
    )
 }
 
-export default Post;
+export default ContentDetail;
 
 
 
-// import React from 'react'
-// import { useRouter } from 'next/router'
-// import { GetStaticProps, NextPage, GetStaticPaths } from 'next'
-// import Image from 'next/image'
-// import Link from 'next/link'
-// import { ParsedUrlQuery } from 'querystring'
-// import { getDatabase, getPages, getBlocks} from '../../library/notion'
-
-
-
-// function ContentDetail({ contents }: any) {
-//   const router = useRouter()
-//   const contentId = router.query.id
-//   const content = contents.results.find(( content:any ) => content.id === contentId)
-//   console.log(content)
   
 //   return (
-//     <section className='w-2/3 mx-auto my-12'>
-//       <div className='w-full mx-auto mt-12 px-16'>
-//         <Image 
-//           src={content.cover.external.url}
-//           alt="Content Image"
-//           width={900}
-//           height={400}
-//         />
-//         <div className='flex mt-8 justify-between items-center'>
-//           <span className='text-2xl font-semibold'>{content.properties.Name.title[0].text.content}</span> 
-//           <span className='text-base text-[#ccc] font-thin'>#{content.properties.Series.select.name}</span>
-//       </div>
-//       <div className='mt-5 flex justify-between'>
-//         <div className='flex'>
-//             {content.properties.Keyword.multi_select.map((keyword: any, index: number) => {
-//               return (
-//                 <span key={index} className="px-2 bg-slate-100 text-[#333] font-semobold rounded-md mr-2">{keyword.name}</span>
-//               )
-//             })}
-//         </div>
-//         <span className='text-base text-[#ccc]'>{content.properties.Date.date.start}</span>
-//       </div>
-//       </div>
-//     </section>
+
 //   )
 // }
 
