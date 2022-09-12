35f2d33a8cac7db351006d662e62a94b101d162f
Resister-boy <hassanpumped17@gmail.com>
2022-09-12 20:43:41 +0900

    Notion library test

--- library/notion.ts
+++ library/notion.ts
@@ -1,12 +1,26 @@
 import { Client } from "@notionhq/client";
 
 const notion = new Client({
-  auth: process.env.BLOG_DATABASE_ID
+  auth: process.env.BLOG_DATABASE_APIKEY
 })
 
 export const getDatabase = async() => {
-  const getContents = await notion.databases.query({
+  const getContentsDatabase= await notion.databases.query({
     database_id: process.env.BLOG_DATABASE_ID
   })
-  return getContents
+  return getContentsDatabase
+}
+
+export const getPages = async(id: string) => {
+  const getContentPage = await notion.pages.retrieve({
+    page_id: id
+  })
+  return getContentPage
+}
+
+export const getBlocks = async(id: string) => {
+  const getContentBlock = await notion.blocks.children.list({
+    block_id: id
+  })
+  return getContentBlock
 }
--- pages/text.tsx
+++ pages/text.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import { getDatabase } from '../library/notion'
 
-function text({ contents }: any) {
-  console.log(contents)
+interface Props {
+  getDatabase: [any]
+}
+
+function text<Props>({ getDatabase }) {
+
+  console.log(getDatabase)
   return (
     <div>text</div>
   )
@@ -9,22 +15,14 @@ function text({ contents }: any) {
 
 export async function getServerSideProps() {
 
-  const options = {
-    method: 'GET',
-    headers: {
-      Accept: 'application/json', 
-      'Notion-Version': '2022-02-22',
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${process.env.PROJECT_DATABASE_APIKEY}`
-    }
-  };
-  
-  const response = await fetch('https://api.notion.com/v1/pages/cc39e3ff-d7f0-420e-8aff-3b6e52984123', options)
-  const contents = await response.json()
+  const { results } = await getDatabase()
 
   return {
-    props: { contents }
+    props: {
+      getDatabase: results
+    }
   }
+
 }
 
 export default text
